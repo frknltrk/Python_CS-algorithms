7f205c3901fc2348cbab3e6399763c3bf12738b5
Furkan Ünlütürk <furkanunluturk@protonmail.com>
2020-01-08 22:32:11 +0300

    refactored

--- BFS.py
+++ BFS.py
@@ -1,29 +1,35 @@
 graph = {}
 
+
 def create_graph(**graph):
     return graph
 
+
 def adjacents(vertex):
     return graph[vertex]
+
 
 def BFA(source):
     distances = {}
     for vertex in graph.keys():
         distances[vertex] = 0
 
-    OPEN = []   #       <-- QUEUE (FIFO) <--
-    CLOSED = [] #       LIST <--
+    OPEN = []    # <-- QUEUE (FIFO) <--
+    CLOSED = []  # LIST <--
 
     OPEN.append(source)
 
     while len(OPEN) != 0:
         x = OPEN.pop(0)
         CLOSED.append(x)
-        for adjacent in adjacents(x):           # ***
-            if (adjacent not in OPEN) and (adjacent not in CLOSED):     # if the vertex (adjacent) is "brand-new"
-                OPEN.append(adjacent)           # ***
+        for adjacent in adjacents(x):
+            # if the vertex (adjacent) is "brand-new"
+            if (adjacent not in OPEN) and (adjacent not in CLOSED):
+                OPEN.append(adjacent)
                 distances[adjacent] = distances[x] + 1
     return distances
 
-graph = create_graph(A=['B', 'C'], B=['D', 'E'], C=['F'], D=['H'], E=[], F=['G'], G=['H'], H=[])
+
+graph = create_graph(A=['B', 'C'], B=['D', 'E'], C=['F'], D=[
+                     'H'], E=[], F=['G'], G=['H'], H=[])
 print(BFA('A'))
--- DFS.py
+++ DFS.py
@@ -1,29 +1,35 @@
 graph = {}
 
+
 def create_graph(**graph):
     return graph
 
+
 def adjacents(vertex):
     return graph[vertex]
+
 
 def BFA(source):
     distances = {}
     for vertex in graph.keys():
         distances[vertex] = 0
 
-    OPEN = []   #       <-- & --> LIST (LIFO)
-    CLOSED = [] #       LIST <--
+    OPEN = []    # <-- & --> LIST (LIFO)
+    CLOSED = []  # LIST <--
 
     OPEN.append(source)
 
     while len(OPEN) != 0:
         x = OPEN.pop(0)
         CLOSED.append(x)
-        for adjacent in reversed(adjacents(x)): # ***
-            if (adjacent not in OPEN) and (adjacent not in CLOSED):     # if the vertex (adjacent) is "brand-new"
-                OPEN.insert(0, adjacent)        # ***
+        for adjacent in reversed(adjacents(x)):
+            # if the vertex (adjacent) is "brand-new"
+            if (adjacent not in OPEN) and (adjacent not in CLOSED):
+                OPEN.insert(0, adjacent)
                 distances[adjacent] = distances[x] + 1
     return distances
 
-graph = create_graph(A=['B', 'C'], B=['D', 'E'], C=['F'], D=['H'], E=[], F=['G'], G=['H'], H=[])
+
+graph = create_graph(A=['B', 'C'], B=['D', 'E'], C=['F'], D=[
+                     'H'], E=[], F=['G'], G=['H'], H=[])
 print(BFA('A'))
